8b90d5e1979c9d9d57f06a2a26a8d5156a5c834c
ouram <mboivin@student.42.fr>
2020-09-02 19:48:52 +0200

    Use join

--- minirt_converter.py
+++ minirt_converter.py
@@ -13,10 +13,10 @@ def get_triangle(line, d, color):
     i1 = i[1].split('/')[0]
     i2 = i[2].split('/')[0]
     i3 = i[3].split('/')[0]
-    v1 = d[i1][1] + ',' + d[i1][2] + ',' + d[i1][3]
-    v2 = d[i2][1] + ',' + d[i2][2] + ',' + d[i2][3]
-    v3 = d[i3][1] + ',' + d[i3][2] + ',' + d[i3][3]
-    triangle = 'tr ' + v1 + ' ' + v2 + ' ' + v3 + ' ' + color + '\n'
+    vertex1 = ','.join((d[i1]['x'], d[i1]['y'], d[i1]['z']))
+    vertex2 = ','.join((d[i2]['x'], d[i2]['y'], d[i2]['z']))
+    vertex3 = ','.join((d[i3]['x'], d[i3]['y'], d[i3]['z']))
+    triangle = ' '.join(('tr', vertex1, vertex2, vertex3, color)) + '\n'
     return triangle
 
 def parse_polygons(filename, obj_file, d, color):
@@ -30,7 +30,7 @@ def parse_vertices(obj_file, d):
     for line in obj_file:
         if line[:2] == 'v ':
             coord = line.split(' ')
-            d[str(count)] = {1: coord[1], 2: coord[2], 3: coord[3][:-1]}
+            d[str(count)] = {'x': coord[1], 'y': coord[2], 'z': coord[3][:-1]}
             count += 1
     return d
 
